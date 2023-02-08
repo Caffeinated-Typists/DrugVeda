47141ebcaba8e78389d124a85bd36014e33c7d7e
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-02-08 17:50:05 +0530

    Added file to initialise the DB

---
+++ data/database_init.py
@@ -0,0 +1,55 @@
+def create_table_customers()->None:
+    """Creates the table customers"""
+
+def create_table_retailers()->None:
+    """Creates the table retailers"""
+
+def create_table_suppliers()->None:
+    """Creates the table suppliers"""
+
+def create_table_brands()->None:
+    """Creates the table brands"""
+
+def create_table_labs()->None:
+    """Creates the table labs"""
+
+def create_table_categories()->None:
+    """Creates the table categories"""
+
+def create_table_subcategories()->None:
+    """Creates the table subcategories"""
+
+def create_table_products()->None:
+    """Creates the table products"""
+
+def create_table_product_cat_rel()->None:
+    """Creates the junction table for product and category relationship"""
+
+def create_table_orders()->None:
+    """Creates the table order"""
+
+def create_table_product_orders()->None:
+    """Creates the table for product in every order"""
+
+def create_table_tests()->None:
+    """Creates the table tests"""
+
+def create_table_appointments()->None:
+    """Creates the table appointments"""
+
+def create_table_inventory()->None:
+    """Creates the table inventory"""
+
+def create_table_batches()->None:
+    """Creates the table batches"""
+
+def create_table_supply_orders()->None:
+    """Creates the table supply orders"""
+
+def main()->None:
+    """Main function"""
+
+if __name__ == "__main__":
+    main()
+else:
+    raise Exception("This file was not created to be imported")
