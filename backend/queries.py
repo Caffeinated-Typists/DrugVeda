55c452acc775bb344dbd9659b383e04bc5e88e2b
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-03-12 02:48:42 +0530

    Added IDs to for categories in API response

--- backend/queries.py
+++ backend/queries.py
@@ -8,8 +8,8 @@ def connect_to_db()->sqlalchemy.engine.base.Engine:
     engine = sqlalchemy.create_engine(f"mysql+mysqlconnector://{os.environ['MySQL_USER']}:{os.environ['MySQL_PASSWORD']}@{os.environ['MySQL_DB_LINK']}", echo=True)
     return engine
 
-def get_all_categories(engine:sqlalchemy.engine)->list[str]:
+def get_all_categories(engine:sqlalchemy.engine)->list[dict[str: str]]:
     """Returns name of all categories from the database"""
     with orm.Session(engine) as session:
         categories = session.query(entities.Category).all()
-        return [category.Name for category in categories]
+        return [{category.CategoryID: category.Name} for category in categories]
