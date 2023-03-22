96760f88bc0902fe17381e762c81c2bd0a6b675d
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-03-23 00:05:35 +0530

    Load DB creds from json file

--- backend/queries.py
+++ backend/queries.py
@@ -3,8 +3,19 @@ import sqlalchemy
 import sqlalchemy.orm as orm
 import data.entities as entities
 
+def load_creds()->None:
+    """Loads the credentials from the .json fill, if they are not already loaded"""
+    if "MySQL_USER" not in os.environ:
+        import json
+        with open("credentials.json", "r") as f:
+            creds = json.load(f)
+            os.environ["MySQL_USER"] = creds["user"]
+            os.environ["MySQL_PASSWORD"] = creds["pwd"]
+            os.environ["MySQL_DB_LINK"] = creds["db_url"]
+
 def connect_to_db()->sqlalchemy.engine.base.Engine:
     """Connect to the database and return the engine"""
+    load_creds()
     engine = sqlalchemy.create_engine(f"mysql+mysqlconnector://{os.environ['MySQL_USER']}:{os.environ['MySQL_PASSWORD']}@{os.environ['MySQL_DB_LINK']}", echo=True)
     return engine
 
